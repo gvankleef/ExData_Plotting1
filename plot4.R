b83c1aa19e514b3e2a21f8ad33dede534d72c161
gvankleef <guido@digitalanalyst.nl>
2019-05-21 17:49:38 +0200

    - plot3 change in ylab text - plot4.R and plot4.png

--- plot4.R
+++ plot4.R
@@ -1,14 +1,3 @@
-##Loading packages
-library(downloader)
-library(lubridate)
-
-##Downloading data
-if(!dir.exists("./data")){dir.create("./data")}
-url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
-download(url, dest="./data/dataset.zip", mode="wb") 
-unzip ("./data/dataset.zip", exdir = "./data")
-
-##Reading data
 data <- read.csv("./data/household_power_consumption.txt", header=T, sep=';', na.strings="?", 
                  nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
 
@@ -21,14 +10,27 @@ datetime <- paste(as.Date(data_feb$Date), data_feb$Time)
 data_feb$Datetime <- as.POSIXct(datetime)
 
 
-## Plot 3
+## Plot 4
+par(mfcol = c(2,2), mar = c(4,4,1,1))
+with(data_feb, {
+        plot(Global_active_power~Datetime, type="l",
+             ylab="Global Active Power", xlab="")
+})
 with(data_feb, {
         plot(Sub_metering_1~Datetime, type="l",
              ylab="Energy sub metering", xlab="", col = "black")
         points(Sub_metering_2~Datetime, type="l", col = "red")
         points(Sub_metering_3~Datetime, type="l", col = "blue")
         legend("topright", col = c("black","red","blue"),
-               legend = c("Sub metering_1","Sub metering_2","Sub metering_3"), lty=1,lwd=2)
+               legend = c("Sub metering_1","Sub metering_2","Sub metering_3"),lwd=1, bty = "n")
 })
-dev.copy(png,'plot3.png', width=480, height=480)
+with(data_feb, {
+        plot(Voltage~Datetime, type="l",
+             ylab="Voltage", xlab="")
+})
+with(data_feb, {
+        plot(Global_reactive_power~Datetime, type="l",
+             , xlab="")
+})
+dev.copy(png,'plot4.png', width=480, height=480)
 dev.off()
