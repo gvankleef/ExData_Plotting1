f47a85588ad0518f970c2dc6c9c95b204f5d07a8
gvankleef <guido@digitalanalyst.nl>
2019-05-21 16:42:51 +0200

    No newline at the end of the file of plot2.R

--- plot2.R
+++ plot2.R
@@ -25,3 +25,5 @@ with(data, {
         plot(Global_active_power~Datetime, type="l",
              ylab="Global Active Power (kilowatts)", xlab="")
 })
+dev.copy(png,'plot2.png', width=480, height=480)
+dev.off()
